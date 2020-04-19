db4cb9ef1cedef622f24fff1b801c4b18a36992f
Ionut Simion <ionut@simion.co>
2020-04-20 00:28:33 +0300

    Print square root if a number is added as parameter

--- src/tutorial.cxx
+++ src/tutorial.cxx
@@ -2,6 +2,11 @@
 
 #include <config.hxx>
 
+double sqr(double x) noexcept
+{
+    return x * x;
+}
+
 int main(int argc, char **argv)
 {
     using namespace is;
@@ -11,7 +16,13 @@ int main(int argc, char **argv)
         std::cout << argv[0]
                   << "\nCMake Tutorial v"
                   << version::major << '.' << version::minor << '.' << version::patch
+                  << "\nUsage:\n\tTutorial <x>, where x is a double;\n\tReturns the square root of <x>"
                   << std::endl;
+    }
+    else
+    {
+        auto const x = std::stod(argv[1]);
+        std::cout << x << "^2 = " << sqr(x) << std::endl;
     }
 
     return 0;
