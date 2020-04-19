4497614c129c3380ab548325e19b3860d586b175
Ionut Simion <ionut@simion.co>
2020-04-20 00:06:54 +0300

    Added C++ standard, added bin (build folder) folder nad vim swp files to ignore

--- src/tutorial.cxx
+++ src/tutorial.cxx
@@ -1,8 +1,19 @@
 #include <iostream>
 
-int main()
+#include <config.hxx>
+
+int main(int argc, char **argv)
 {
-    std::cout << "Hello, CMake!" << std::endl;
+    using namespace is;
+
+    if (argc < 2)
+    {
+        std::cout << argv[0]
+                  << "\nCMake Tutorial v"
+                  << version::major << '.' << version::minor << '.' << version::patch
+                  << std::endl;
+    }
+
     return 0;
 }
 
