c372876f1563de0573fc93be6f6cd8ba654c45b2
Ionut Simion <ionut@simion.co>
2021-01-02 23:16:17 +0200

    Added tests (step 4)

--- lib/math/src/math.cxx
+++ lib/math/src/math.cxx
@@ -1,3 +1,5 @@
+#include <cstdint>
+
 #include <math.hxx>
 
 namespace is
@@ -9,6 +11,7 @@ namespace is
         auto i{ *reinterpret_cast<int32_t *>(const_cast<float *>(&x)) };
         i = 0x5f3759df - (i >> 1);
         auto y{ *reinterpret_cast<float *>(&i) };
+        y *= three_halfs - (half_x * y * y);
         
         return 1.0f / (y * (three_halfs - (half_x * y * y)));
     }
