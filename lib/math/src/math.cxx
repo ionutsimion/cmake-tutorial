30e61778510819ac4a51b08b9463d6926d0fec1c
Ionut Simion <ionut@simion.co>
2021-01-03 14:34:47 +0200

    Added custom command and generated file (step 6)

--- lib/math/src/math.cxx
+++ lib/math/src/math.cxx
@@ -1,7 +1,10 @@
 #include <cstdint>
 #include <cmath>
 
+#include <iostream>
+
 #include <math.hxx>
+#include <table.hxx>
 
 namespace is
 {
@@ -21,10 +24,34 @@ namespace is
         
         return 1.0f / (y * (three_halfs - (half_x * y * y)));
     }
+
+    inline double square_root_from_table(double const x)
+    {
+        auto f{ sqrt_table[static_cast<int>(x)] };
+
+        // do 10 iterations
+        for (auto i = 0; i < 10; ++i)
+        {
+            if (f <= 0)
+                f = 0.1;
+            auto const delta = x - (f * f);
+            f += 0.5 * delta / f;
+        }
+
+        return f;
+    }
     
     double square_root(double const x)
     {
+        if (x <= 0.0)
+            return 0;
+
 #if defined(HAVE_LOG) and defined(HAVE_EXP)
+        if (x < 10)
+        {
+            std::cout << "Using the square roots table..." << std::endl;
+            return square_root_from_table(x);
+        }
         return exp_log_square_root(x);
 #endif
         return quake_reverse_square_root(x);
